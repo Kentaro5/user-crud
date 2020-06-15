1acd917a48fceb48666699fa658b659342cbfce8
Kentaro Minami <mkentaro555@gmail.com>
2020-06-15 19:40:10 +0900

    MySQLエラーの箇所修正。

--- src/Controller/Component/User/UserComponent.php
+++ src/Controller/Component/User/UserComponent.php
@@ -58,6 +58,9 @@ class UserComponent extends Component {
         $user = $this->Users->newEntity($request->getData());
         $user = $this->Users->patchEntity($user, $request->getData());
 
+        //Entityに通すとsexが消えるので、改めて追加。
+        $user->sex = $request->getData()['sex'];
+
         //エラーがあれば、errorsに格納。
         if(!empty($this->user_name_form->errors()['first_name'])) {
             $user->errors('first_name', $this->user_name_form->errors()['first_name']);
