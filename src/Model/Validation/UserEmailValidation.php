21fe465bc9bbfbb3742c79acaffda6384dfac93b
Kentaro Minami <mkentaro555@gmail.com>
2020-06-15 14:49:52 +0900

    メールアドレスのカスタムバリデーション作成。

---
+++ src/Model/Validation/UserEmailValidation.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Model\Validation;
+
+use Cake\Validation\Validation;
+
+class UserEmailValidation extends Validation
+{
+
+    public static function checkUserEmail($user_email)
+    {
+        //空白削除
+        $user_email  = preg_replace("/( |　)/", "", $user_email );
+
+        //ユーザーのメールアドレスが空じゃないかどうかをチェックする。
+        if( mb_strlen( $user_email, "UTF-8" ) <= 0 ){
+            return false;
+        }
+
+        return true;
+    }
+
+    public static function checkUserEmailLength($user_email)
+    {
+        //空白削除
+        $user_email  = preg_replace("/( |　)/", "", $user_email );
+
+        //ユーザーの姓名はそれぞれ30文字以内
+        if( mb_strlen( $user_email, "UTF-8" ) > 254 ){
+            return false;
+        }
+
+        return true;
+    }
+
+}
