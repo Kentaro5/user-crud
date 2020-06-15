5aa9cd8a0e0fdc6eca5af303a348a5b89c604011
Kentaro Minami <mkentaro555@gmail.com>
2020-06-15 22:52:44 +0900

    ユーザー情報更新処理作成。

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -86,21 +86,24 @@ class UsersController extends AppController
      * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
-    public function edit($id = null)
+    public function edit($user_id = null)
     {
-        $user = $this->Users->get($id, [
-            'contain' => []
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $user = $this->Users->patchEntity($user, $this->request->getData());
-            if ($this->Users->save($user)) {
-                $this->Flash->success(__('The user has been saved.'));
 
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The user could not be saved. Please, try again.'));
+        //使用するコンポーネントをロード。
+        $this->loadComponent('User',[
+            'className' => '\App\Controller\Component\User\UserComponent'
+        ]);
+
+        $sexs = $this->User->getSexLists();
+        $user = $this->User->getUserInfo($user_id);
+
+        if ($this->request->is(['post', 'put'])) {
+            //ユーザー情報更新処理
+            $user = $this->User->editUserInfo($this->request, $user_id);
         }
-        $this->set(compact('user'));
+
+        $this->set('user', $user);
+        $this->set('sexs', $sexs);
     }
 
     /**
