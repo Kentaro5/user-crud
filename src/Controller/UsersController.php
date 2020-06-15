6cfdb9a7f3d7441677ce0c0e4d732f1dc0e080b6
Kentaro Minami <mkentaro555@gmail.com>
2020-06-16 03:07:39 +0900

    必要のないメソッド削除。

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -38,22 +38,6 @@ class UsersController extends AppController
     }
 
     /**
-     * View method
-     *
-     * @param string|null $id User id.
-     * @return \Cake\Http\Response|void
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function view($id = null)
-    {
-        $user = $this->Users->get($id, [
-            'contain' => []
-        ]);
-
-        $this->set('user', $user);
-    }
-
-    /**
      * Add method
      *
      * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
